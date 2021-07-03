0845a8081e9a876abbb60ff19edd4247968d0f62
Herman Saksono <herman.saksono@gmail.com>
2021-07-03 09:09:02 -0400

    Make the map take the full width of the page

--- static/scripts/boston_unemployment_vis.js
+++ static/scripts/boston_unemployment_vis.js
@@ -1,9 +1,9 @@
 const MAP_WIDTH = 900;
 const MAP_HEIGHT = 700;
 const EXCLUDED_TRACTS = ["25025990101", "25025980101", "25025981501"];
-const EXCLUDED_NEIGHBORHOODS = ["Bay Village", "Leather District", "Chinatown", "Waterfront"];
-const CITY_CENTER = [-71.065, 42.357];
-const INITIAL_SCALE = 140000;
+const EXCLUDED_NEIGHBORHOODS = ["Bay Village", "Leather District", "Chinatown", "Waterfront", "West End"];
+const CITY_CENTER = [-70.970, 42.329];
+const INITIAL_SCALE = 94500;
 const LABEL_FONT_SIZE = 0.55;
 const DIV_ID_FOR_SVG_MAP = "div#mapSvgContainer";
 
